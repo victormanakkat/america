7d7df2334a58613f6e4c71a491fafca2ae8f5bfb
Tyler Spadgenske <tyspadgens@gmail.com>
2014-05-16 17:29:33 -0500

    Added day and night

--- code/level.py
+++ code/level.py
@@ -41,19 +41,30 @@ class Levels():
         self.direction = 'S'
         self.time = [0,0,100]
         self.go = False
+        self.sun = -2
         self.firstSound = False
         pygame.mixer.music.load('data\\sound\\Godbless.mid')
-        
+        self.quit = 0
         while True:
+            if self.sun > 8:
+                self.quit = self.layout.blackout(time)
+            if self.sun < 0:
+                self.layout.begin(time,'1620',self.sun)
+            self.layout.sky(self.sun)
             self.x, self.y = self.stay_on_screen(self.x,self.y)
-            self.layout.blackout(self.time)
             self.layout.drawMap()
-            self.layout.sky()
             self.x, self.y, self.go = self.man.move(self.x,self.y,self.direction,self.time,self.go)
             self.WilliamBradford.move(self.coords['WilliamBradford'][0],self.coords['WilliamBradford'][1],'S',self.time,False,1)
             self.WilliamBradford.check_for_chat(self.time, self.x, self.y, self.coords['WilliamBradford'][0], self.coords['WilliamBradford'][1])
             self.end, self.sound, self.pause, self.help = self.settings.settings_button()
-        
+            if self.sun > 8:
+                self.quit = self.layout.blackout(time)
+            if self.sun < 0:
+                self.layout.begin(time, '1620',self.sun)
+
+            if self.quit > 254:
+                break
+                
             for event in pygame.event.get():
                 self.settings.settings_button(event)
                 self.WilliamBradford.check_for_chat(self.time, self.x, self.y, self.coords['WilliamBradford'][0], self.coords['WilliamBradford'][1], event)
@@ -72,6 +83,7 @@ class Levels():
                 self.time[1] = 0
             if self.time[2] == 200:
                 self.time[2] = 0
+                self.sun += 1
             self.time[0] += 1
             self.time[1] += 1
             self.time[2] += 1
--- code/map.py
+++ code/map.py
@@ -3,29 +3,49 @@ from pygame.locals import *
 
 pygame.init()
 
-WINDOWWIDTH = 1184
-WINDOWHIEGHT = 640
-os.environ ['SDL_VIDEO_WINDOW_POS'] = 'center'
-surface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHIEGHT), 0, 32)
-pygame.display.set_caption('Test')
-mainClock = pygame.time.Clock()
-
 SKY_BLUE = (0, 255, 255)
 WHITE = (255, 255, 255)
-
-surface.fill(WHITE)
 
 class Map():
     def __init__(self, surface):
         self.screen = surface
         self.BLACK = (0,0,0, 255)
         self.num = 0
+        self.num2 = 255
         self.dim = pygame.Surface((800,608))        
-        self.dim.fill(self.BLACK)  
+        self.dim.fill(self.BLACK)
+        self.YELLOW = (255,255,0)
+        self.WHITE = (255,255,255)
+        self.font = pygame.font.Font('data\\fonts\\font1.ttf', 24)
+        self.first = True
 
-    def sky(self):
+
+    def sky(self,sun):
         block = {'rect':pygame.Rect(0, 0, 800, 130),'color':SKY_BLUE}
-        pygame.draw.rect(surface, block['color'], block['rect'])
+        pygame.draw.rect(self.screen, block['color'], block['rect'])
+
+        if sun == 8:
+            self.coords = [600,100]
+        elif sun == 7:
+            self.coords = [550,75]
+        elif sun == 6:
+            self.coords = [500,50]
+        elif sun == 5:
+            self.coords = [450, 25]
+        elif sun == 4:
+            self.coords = [400, 0]
+        elif sun == 3:
+            self.coords = [350,25]
+        elif sun == 2:
+            self.coords = [300,50]
+        elif sun == 1:
+            self.coords = [250, 75]
+        elif sun == 0:
+            self.coords = [200,100]
+        else:
+            self.coords = [1000,1000]
+
+        pygame.draw.circle(self.screen, self.YELLOW, self.coords, 40, 0)
         
     def drawMap(self, mapPath='data\\levels\\test.txt'):
         self.map = open(mapPath, 'r')
@@ -45,15 +65,34 @@ class Map():
                 self.rect.centerx = x * 32
                 self.rect.centery = y * 32
                 if tile == '0':
-                    surface.blit(self.grass, self.rect)
+                    self.screen.blit(self.grass, self.rect)
                 if tile == '1':
-                    surface.blit(self.rock, self.rect)
+                    self.screen.blit(self.rock, self.rect)
                 x += 1
             y += 1
             x = 0
 
     def blackout(self, time):
-        self.num += 100
+        self.num += .1
         self.dim.set_alpha(self.num)
-        self.screen.blit(self.dim, (0,0)) 
+        self.screen.blit(self.dim, (0,0))
+        return self.num
+
+    def begin(self, time, year,sun):
+        if sun == -1:
+            self.first = False
+        if self.first:
+            self.year = self.font.render(year, True, self.WHITE, self.BLACK)
+            self.yearRect = self.year.get_rect()
+            self.yearRect.centerx = 400
+            self.yearRect.centery = 300
+            self.screen.blit(self.dim, (0,0)) 
+            self.screen.blit(self.year, self.yearRect)
+            
+        else:
+            self.num2 -= 1
+            self.dim.set_alpha(self.num2)
+            self.screen.blit(self.dim, (0,0))
+
+        
         
